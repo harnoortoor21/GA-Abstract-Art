c27eab43a24e4ea8a497215b8f3724f78e631285
manivir421 <manivir.3201@gmail.com>
2026-04-16 14:09:43 -0700

    added fitness function

--- src/fitness.py
+++ src/fitness.py
@@ -13,3 +13,19 @@ the evolution process toward better image representations.
 
 Higher fitness scores indicate a better match between music and image.
 """
+
+def check_fitness(chromosome, target):
+# get the features from chromosome
+  energy, density, valence = get_features(chromosome)
+
+# calculate the difference between scores from chromosome and target value(based on midi file), taking absolute value and subtracting
+# from 1 so that higher the difference between chromosome and target , lower the fitness of the chromosome
+  e_score = 1- abs(energy - target["energy"])
+  d_score = 1- abs(density - target["density"])
+  v_score = 1- abs(valence - target["valence"])
+
+  avg = (e_score, d_score, v_score) / 3
+  
+# using exponential in fitness to check if fitness increases and reaches closer to target faster 
+  fitness = avg**2
+  return fitness
